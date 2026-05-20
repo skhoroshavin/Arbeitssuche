d71d96c8cc79ef0e4d003c8ce74de5278163488f
Sergey Khoroshavin <skhoroshavin@gmail.com>
2026-05-20 17:16:42 +0200

    refactor: separate unit and integration test configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   },
   test: {
     include: ["{src,scripts,eslint}/**/*.test.{ts,tsx}"],
+    exclude: ["**/integration.test.ts"],
     setupFiles: ["./src/ui/test-setup.ts"],
   },
 })
